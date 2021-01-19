e0238930e9963424b88a18fa34d2b019867e1d79
havadora <71092442+havadora@users.noreply.github.com>
2021-01-19 18:34:57 -0500

    Main.cpp

--- GAME1011_W2021_AS1_LeNguyenTruongHai/Main.cpp
+++ GAME1011_W2021_AS1_LeNguyenTruongHai/Main.cpp
@@ -8,54 +8,57 @@ class Platform
 {
 private:
 
-	string platformName;
-	string Manufactor;
-	string Games;
+	string m_PlatformName;
+	string m_Manufactor;
+	string m_Games[3];
 
 public:
 	Platform()
 	{
-		platformName = "NONE";
-		Manufactor   = "NONE";
-		Games        = "NONE";
+		m_PlatformName = "NONE";
+		m_Manufactor   = "NONE";
+		this->m_Games[3]  = "NONE";
 	}
 	Platform(string PlatName, string Manufactors, string Game)
 	{
-		platformName = PlatName;
-		Manufactor = Manufactors;
-		Games = Game;
+		m_PlatformName = PlatName;
+		m_Manufactor = Manufactors;
+		this->m_Games[3] = Game;
 	}
 	string GetPlatName()
 	{
-		return platformName;
+		return m_PlatformName;
 	}
 	string GetManu()
 	{
-		return Manufactor;
+		return m_Manufactor;
 	}
 };
 
-class GAME
+class GAME : public Platform
 {
 private:
 
-	string GameName;
-	string Publisher;
-	string Developer;
+	string m_GameName;
+	string m_Publisher;
+	string m_Developer;
+	string m_Achievement[3];
 
 public:
 
 	GAME()
 	{
-		GameName = "NONE";
-		Publisher = "NONE";
-		Developer = "NONE";
+		m_GameName = "NONE";
+		m_Publisher = "NONE";
+		m_Developer = "NONE";
+		this->m_Achievement[3] = "NONE";
 	}
-	GAME(string GameNames, string Publishers, string Developers)
+	GAME(string GameNames, string Publishers, string Developers, string Achievements)
 	{
-		GameName  = GameNames;
-		Publisher = Publishers;
-		Developer = Developers;
+		m_GameName = GameNames;
+		m_Publisher = Publishers;
+		m_Developer = Developers;
+		this->m_Achievement[3] = Achievements;
 	}
 
 
