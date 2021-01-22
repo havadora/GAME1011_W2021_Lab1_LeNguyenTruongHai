46d390be34017211bed0d9c5cfeb721281ee11c1
havadora <71092442+havadora@users.noreply.github.com>
2021-01-21 19:17:54 -0500

    Main.cpp

--- GAME1011_W2021_AS1_LeNguyenTruongHai/Main.cpp
+++ GAME1011_W2021_AS1_LeNguyenTruongHai/Main.cpp
@@ -7,18 +7,28 @@ using namespace std;
 
 class Platform
 {
+public:
+	static const int  size = 3;
+	static const int  Game_size = 5;
 private:
-
-	string m_ArrayPlatformName[3] = {"PlayStaion","Xbox","Switch" };
-	string m_ArrayManufactor[3] = { "Sony", "Microsoft", "Nintendo" };
+	
+	string m_ArrayPlatformName[size] = {"PlayStaion","Xbox","Switch" };
+	string m_ArrayManufactor[size] = { "Sony", "Microsoft", "Nintendo" };
 	string m_PlatformName;
 	string m_Manufactor;
 protected:
-	string m_ArrayPSGames[5] = { "God Of War","Detroit:Become Human","The Last Of Us Part II", "Marvel's Spidermam", "Ghost of Tsushima"};
-	string m_ArrayXboxGames[5] = { "Quantum Break","Sea of Thieves","Ori and the Will of the Wisps","Ashen","Gears 5" };
-	string m_ArraySwitchGames[5] = {"Pokemon Sword", "Pokemon Shield","Animal Crossing","Super Smash Bro: Ultimate","The Legend Of Zelda: Breath of the Wild"};
+	string m_ArrayPSGames[Game_size] = { "God Of War","Detroit:Become Human","The Last Of Us Part II", "Marvel's Spidermam", "Ghost of Tsushima"};
+	string m_ArrayXboxGames[Game_size] = { "Quantum Break","Sea of Thieves","Ori and the Will of the Wisps","Ashen","Gears 5" };
+	string m_ArraySwitchGames[Game_size] = {"Pokemon Sword", "Pokemon Shield","Animal Crossing","Super Smash Bro: Ultimate","The Legend Of Zelda: Breath of the Wild"};
 public:
-	
+	void PlatformPrint() const
+	{
+		for (int i = 0; i < size; i++)
+		{
+			cout << i+1 <<". " << m_ArrayPlatformName[i] << endl;
+
+		}
+	}
 	
 	string GetPlatName(int choices)
 	{
@@ -35,24 +45,45 @@ public:
 
 class GAME : public Platform
 {
+public:
+	static const int  Publicsher_Size = 3;
+	static const int  Dev_size = 5;
 private:
 	// Publisher
-	string m_PSPublisher[1] = {"Sony Interactive Entertainment"};
-	string m_XboxPublisher[1] = {"Xbox Game Studios"};
-	string m_SwitchPublisher[1] = {"Nintendo"};
+	string m_ArrayPublisher[Publicsher_Size] = {"Sony Interactive Entertainment","Xbox Game Studios","Nintendo" };
 	//Developer
-	string m_PSDeveloper[5] = { "Santa Monica Studio","Quantic Dream","Naughty Dog","Insomniac Games","Sucker Punch Productions" };
-	string m_XboxDeveloper[5] = { "Remedy Entertainment","Rare LTD","Moon Studios","A44","The Coalition" };
-	string m_SwitchDeveloper[5] = {"GAME FREAK Inc","GAME FREAK Inc","Nintendo","Bandai Namco Studio","Nintendo" };
+	string m_PSDeveloper[Dev_size] = { "Santa Monica Studio","Quantic Dream","Naughty Dog","Insomniac Games","Sucker Punch Productions" };
+	string m_XboxDeveloper[Dev_size] = { "Remedy Entertainment","Rare LTD","Moon Studios","A44","The Coalition" };
+	string m_SwitchDeveloper[Dev_size] = {"GAME FREAK Inc","GAME FREAK Inc","Nintendo","Bandai Namco Studio","Nintendo" };
 
 	string m_GameName;
 	string m_Publisher;
 	string m_Developer;
-protected:
-	string m_Achievements[5];
-
 public:
-
+	void GamePrint(int choices) const
+	{
+		switch (choices)
+		{
+		case 1:
+			for (int i = 0; i < Game_size; i++)
+			{
+				cout << i + 1 << ". " << m_ArrayPSGames[i] << endl;
+			}
+			break;
+		case 2:
+			for (int i = 0; i < Game_size; i++)
+			{
+				cout << i + 1 << ". " << m_ArrayXboxGames[i] << endl;
+			}
+			break;
+		case 3:
+			for (int i = 0; i < Game_size; i++)
+			{
+				cout << i + 1 << ". " << m_ArraySwitchGames[i] << endl;
+			}
+			break;
+		}
+	}
 	
 	string GetGameNames(int choices, int Gamechoices)
 	{
@@ -72,18 +103,7 @@ public:
 	}
 	string GetPublisher(int choices)
 	{
-		switch (choices)
-		{
-		case 1:
-			m_Publisher = m_PSPublisher[1];
-			break;
-		case 2:
-			m_Publisher = m_XboxPublisher[1];
-			break;
-		case 3:
-			m_Publisher = m_SwitchPublisher[1];
-			break;
-		}
+		m_Publisher = m_ArrayPublisher[choices];
 		return m_Publisher;
 	}
 	string GetDeveloper(int choices, int Developerchoices)
@@ -94,10 +114,10 @@ public:
 			m_Developer = m_PSDeveloper[Developerchoices];
 			break;
 		case 2:
-			m_Developer = m_ArrayXboxGames[Developerchoices];
+			m_Developer = m_XboxDeveloper[Developerchoices];
 			break;
 		case 3:
-			m_Developer = m_ArraySwitchGames[Developerchoices];
+			m_Developer = m_SwitchDeveloper[Developerchoices];
 			break;
 		}
 		return m_Developer;
@@ -106,7 +126,138 @@ public:
 };
 class ACHIEVEMENT : public GAME
 {
+private:
+	string m_Tilte;
+	string m_Decsription;
+	int m_ScoreValue;
+	string* m_PtrTitle = nullptr;
+	string* m_PtrDescription = nullptr;
+	int* m_PtrScoreValue = nullptr;
+	int m_ArraySize;
+	
+public:
+	ACHIEVEMENT()
+	{
+		m_Tilte = "NONE";
+		m_Decsription = "NONE";
+		m_ScoreValue = 0;
+	}
+	ACHIEVEMENT(string Tilte,  string Decsription, int ScoreValue, int Size, int order) 
+	{
+		
+		SetTitle(Tilte,Size,order);
+		SetDecsription(Decsription,Size, order);
+		SetValue(ScoreValue,Size,order);
+	}
+	void SetTitle(string title,int Size, int order)
+	{	
+		
+		m_PtrTitle = new string[Size];
+		m_PtrTitle[order] = title;
+	}
+	void SetDecsription(string descrpition, int Size, int order)
+	{
+		m_PtrDescription = new string[Size];
+		m_PtrDescription[order] = descrpition ;
+	}
+	void SetValue(int value, int Size, int order)
+	{
+		
+		m_PtrScoreValue[order] = value;
+	}
+	void print(int size)
+	{
+		for (int i = 0; i < size; i++)
+		{
+			cout << "Achievement " << i + 1 << endl;
+			cout << "Title: " << m_PtrTitle[i] << endl;
+			cout << "Description: " << m_PtrDescription[i] << endl;
+			cout << "Score Value: " << m_PtrScoreValue[i] << endl;
+		}
 
-
+	}
+	
 
 };
+
+
+int main()
+{
+	Platform platform;
+	GAME Game;
+	ACHIEVEMENT ach;
+
+	int Platformchoices;
+	int Gamechoices;
+	int AchievementSize;
+	string title;
+	string description;
+	int value;
+	bool options = true;
+	int OPnumber;
+
+	cout << "What is the platform you want to choose (Enter the number) ? " << endl;
+	platform.PlatformPrint();
+	cin >> Platformchoices;
+
+	cout << "====================================================================" << endl;
+	cout << "The Platform: " << platform.GetPlatName(Platformchoices-1) << endl;
+	cout << "The Manufacturer: " << platform.GetManu(Platformchoices-1) << endl;
+	cout << "====================================================================" << endl;
+	cout << endl;
+
+	cout << "Chose 1 of 5 exclusive games on " << platform.GetPlatName(Platformchoices - 1) << " (Enter the number) ?" << endl;
+	Game.GamePrint(Platformchoices);
+	cin >> Gamechoices;
+
+	cout << "====================================================================" << endl;
+	cout << "The Game: " << Game.GetGameNames(Platformchoices, Gamechoices-1) << endl;
+	cout << "The Publisher: " << Game.GetPublisher(Platformchoices-1) << endl;
+	cout << "The Developer: " << Game.GetDeveloper(Platformchoices, Gamechoices-1) << endl;
+	cout << "====================================================================" << endl;
+	cout << endl;
+
+	cout << "How many achievements do u want in " << Game.GetGameNames(Platformchoices, Gamechoices - 1) << " ?" << endl;
+	cin >> AchievementSize;
+
+	while (options == true)
+	{
+		for (int i = 0; i < AchievementSize; i++)
+		{
+			cin.ignore();
+			cout << "Achivement " << i+1 << endl;
+			cout << "Enter the title: ";
+			getline(std::cin, title);
+			cout << "Enter the description: ";
+			getline(std::cin, description);
+			cout << "Enter the score value: ";
+			cin >> value;
+			
+			ach.SetTitle(title,AchievementSize,i);
+			ach.SetDecsription(description, AchievementSize, i);
+			ach.SetValue(value, AchievementSize, i);
+		}
+		cin.ignore();
+		cout << "Do you want to add more ? (1-yes|0-no) ";
+		cin >> OPnumber;
+		if (OPnumber == 1)
+		{
+			cout << "How many achievements do u want in " << Game.GetGameNames(Platformchoices, Gamechoices - 1) << " ?" << endl;
+			cin >> AchievementSize;
+		}
+		else
+		{
+			options = false;
+		}
+	}
+	cout << endl;
+	cout << "==============================================================================" << endl;
+	cout << "The Platform: " << platform.GetPlatName(Platformchoices - 1) << endl;
+	cout << "The Manufacturer: " << platform.GetManu(Platformchoices - 1) << endl;
+	cout << "The Game: " << Game.GetGameNames(Platformchoices, Gamechoices - 1) << endl;
+	cout << "The Publisher: " << Game.GetPublisher(Platformchoices - 1) << endl;
+	cout << "The Developer: " << Game.GetDeveloper(Platformchoices, Gamechoices - 1) << endl;
+	ach.print(AchievementSize);
+	cout << "==============================================================================" << endl;
+
+}
